f6611df5e813a5cc32a4dc4b3a53c381db3eea83
Mohammed Sameer <msameer@foolab.org>
2011-09-18 23:32:54 +0300

    Proper playback support. libplayback support. Flip to stop playing

--- common/recitations.cpp
+++ common/recitations.cpp
@@ -3,26 +3,66 @@
 #include <QDebug>
 #include "settings.h"
 #include <QMediaPlayer>
+#include "audiopolicy.h"
+#include "mediaplaylist.h"
+#include <QOrientationSensor>
+#include <QOrientationReading>
 
 Recitations::Recitations(const QString& dir, Downloader *downloader, Settings *settings,
 			 DataProvider *data, QObject *parent)
   : QObject(parent), m_downloader(downloader), m_dir(dir), m_settings(settings),
-    m_data(data), m_player(0), m_recitation(0), m_current(0) {
+    m_data(data), m_player(0), m_playlist(0), m_recitation(0), m_current(0),
+    m_policy(new AudioPolicy(this)), m_play(false),
+    m_chapter(-1), m_verse(-1) {
 
+  QObject::connect(m_policy, SIGNAL(acquired()), this, SLOT(policyAcquired()));
+  QObject::connect(m_policy, SIGNAL(lost()), this, SLOT(policyLost()));
+  QObject::connect(m_policy, SIGNAL(denied()), this, SLOT(policyDenied()));
+
+  m_sensor = new QOrientationSensor(this);
+
+  QObject::connect(m_sensor, SIGNAL(readingChanged()), this, SLOT(sensorReadingChanged()));
 }
 
 Recitations::~Recitations() {
   qDeleteAll(m_installed.values());
   m_installed.clear();
 
+  m_sensor->stop();
+
   if (m_player) {
     m_player->stop();
   }
+
+  delete m_playlist;
+  m_playlist = 0;
 
   delete m_player;
   m_player = 0;
 
   m_recitation = 0;
+}
+
+int Recitations::chapter() const {
+  return m_chapter;
+}
+
+int Recitations::verse() const {
+  return m_verse;
+}
+
+void Recitations::setChapter(int chapter) {
+  if (chapter != m_chapter) {
+    m_chapter = chapter;
+    emit chapterChanged();
+  }
+}
+
+void Recitations::setVerse(int verse) {
+  if (m_verse != verse) {
+    m_verse = verse;
+    emit verseChanged();
+  }
 }
 
 int Recitations::current() const {
@@ -66,13 +106,30 @@ bool Recitations::load(int id) {
     return false;
   }
 
-  if (m_player) {
-    delete m_player;
+  if (!m_player) {
+    m_player = new QMediaPlayer(this);
+
+    m_playlist = new MediaPlaylist(m_settings, m_data, this);
+    QMediaPlaylist *list = m_player->playlist();
+    if (list) {
+      list->deleteLater();
+    }
+
+    m_player->setPlaylist(m_playlist);
   }
 
-  m_player = new QMediaPlayer(this);
+  m_player->stop();
+
+  QObject::connect(m_player, SIGNAL(error(QMediaPlayer::Error)),
+		   this, SLOT(playerError()));
+  QObject::connect(m_player, SIGNAL(stateChanged(QMediaPlayer::State)),
+		   this, SLOT(playerStateChanged()));
+  QObject::connect(m_player, SIGNAL(mediaChanged(const QMediaContent&)),
+		   this, SLOT(playerMediaChanged()));
 
   m_recitation = m_installed[id];
+
+  m_playlist->setRecitation(m_recitation);
 
   m_current = id;
 
@@ -105,10 +162,104 @@ void Recitations::unload() {
     m_player->stop();
   }
 
+  delete m_playlist;
+  m_playlist = 0;
+
   delete m_player;
   m_player = 0;
 
   m_recitation = 0;
+
+  m_sensor->stop();
+}
+
+bool Recitations::isPlaying() const {
+  return m_player ? m_player->state() != QMediaPlayer::StoppedState : false;
+}
+
+void Recitations::playerStateChanged() {
+  emit playingChanged();
+}
+
+void Recitations::playerError() {
+  m_play = false;
+  m_sensor->stop();
+  m_player->stop();
+  emit error(m_player->errorString());
+
+  setChapter(-1);
+  setVerse(-1);
+}
+
+void Recitations::playerMediaChanged() {
+  QString id = QFileInfo(m_player->media().canonicalUrl().toString()).fileName();
+
+  if (id.isEmpty()) {
+    return;
+  }
+
+  int chapter = id.left(3).toInt() - 1;
+  int verse = id.mid(3, 3).toInt() - 1;
+
+  switch (m_playlist->mode()) {
+  case MediaPlaylist::PlayVerse:
+    emit positionChanged(chapter, verse);
+    setChapter(chapter);
+    setVerse(verse);
+    break;
+
+  case MediaPlaylist::PlayPage:
+    // We are playing the first page.
+    if (chapter == 0 && verse == 0 && m_playlist->page() != 0) {
+      setChapter(-1);
+      setVerse(-1);
+
+      break;
+    }
+
+    emit positionChanged(chapter, verse);
+
+    setChapter(chapter);
+    setVerse(verse);
+
+    break;
+
+  case MediaPlaylist::PlayChapter:
+    if (verse == 0 && chapter == 0 && m_playlist->chapter() != 0) {
+      setChapter(-1);
+      setVerse(-1);
+
+      break;
+    }
+
+    emit positionChanged(chapter, verse);
+
+    setChapter(chapter);
+    setVerse(verse);
+
+    break;
+  }
+}
+
+void Recitations::policyAcquired() {
+  if (m_player && m_play) {
+    m_sensor->start();
+    m_player->play();
+  }
+}
+
+void Recitations::stop() {
+  m_play = false;
+
+  if (!m_player || !m_recitation) {
+    return;
+  }
+
+  m_sensor->stop();
+  m_player->stop();
+
+  setChapter(-1);
+  setVerse(-1);
 }
 
 void Recitations::play(int chapter, int verse) {
@@ -116,6 +267,53 @@ void Recitations::play(int chapter, int verse) {
     return;
   }
 
-  m_player->setMedia(m_recitation->mediaUrl(chapter + 1, verse + 1));
-  m_player->play();
+  m_player->stop();
+  m_playlist->playVerse(chapter, verse);
+
+  m_play = true;
+  m_policy->acquire();
+}
+
+void Recitations::playPage(int number) {
+  if (!m_player || !m_recitation) {
+    return;
+  }
+
+  m_player->stop();
+  m_playlist->playPage(number);
+
+  m_play = true;
+  m_policy->acquire();
+}
+
+void Recitations::playChapter(int chapter) {
+  if (!m_player || !m_recitation) {
+    return;
+  }
+
+  m_player->stop();
+  m_playlist->playChapter(chapter);
+
+  m_play = true;
+  m_policy->acquire();
+}
+
+void Recitations::policyDenied() {
+  policyLost();
+  emit error(tr("Audio in use by another application"));
+}
+
+void Recitations::policyLost() {
+  m_play = false;
+
+  if (m_player) {
+    m_sensor->stop();
+    m_player->stop();
+  }
+}
+
+void Recitations::sensorReadingChanged() {
+  if (m_sensor->reading()->orientation() == QOrientationReading::FaceDown) {
+    stop();
+  }
 }
--- common/recitations.h
+++ common/recitations.h
@@ -4,18 +4,30 @@
 #include <QObject>
 #include <QDir>
 #include <QMap>
+#include <qmobilityglobal.h>
 
 class Downloader;
 class Settings;
 class DataProvider;
 class Recitation;
 class QMediaPlayer;
+class AudioPolicy;
+class MediaPlaylist;
+
+QTM_BEGIN_NAMESPACE
+class QOrientationSensor;
+QTM_END_NAMESPACE
+
+QTM_USE_NAMESPACE;
 
 class Recitations : public QObject {
   Q_OBJECT
 
   Q_PROPERTY(QList<int> installed READ installed NOTIFY installedChanged);
   Q_PROPERTY(int current READ current NOTIFY currentChanged);
+  Q_PROPERTY(bool isPlaying READ isPlaying NOTIFY playingChanged);
+  Q_PROPERTY(int chapter READ chapter NOTIFY chapterChanged);
+  Q_PROPERTY(int verse READ verse NOTIFY verseChanged);
 
 public:
   Recitations(const QString& dir, Downloader *downloader, Settings *settings,
@@ -32,16 +44,41 @@ public:
 
   int current() const;
 
+  bool isPlaying() const;
+
+  int chapter() const;
+  int verse() const;
+
 public slots:
   void refresh();
   void play(int chapter, int verse);
   void unload();
+  void playPage(int number);
+  void playChapter(int chapter);
+  void stop();
 
 signals:
   void installedChanged();
   void currentChanged();
+  void playingChanged();
+  void error(const QString& msg);
+  void positionChanged(int chapter, int verse);
+  void verseChanged();
+  void chapterChanged();
+
+private slots:
+  void playerStateChanged();
+  void policyAcquired();
+  void policyLost();
+  void policyDenied();
+  void playerError();
+  void playerMediaChanged();
+  void sensorReadingChanged();
 
 private:
+  void setChapter(int chapter);
+  void setVerse(int verse);
+
   Downloader *m_downloader;
 
   const QDir m_dir;
@@ -52,8 +89,17 @@ private:
   QMap<int, Recitation *> m_installed;
 
   QMediaPlayer *m_player;
+  MediaPlaylist *m_playlist;
   Recitation *m_recitation;
   int m_current;
+
+  AudioPolicy *m_policy;
+  bool m_play;
+
+  int m_chapter;
+  int m_verse;
+
+  QOrientationSensor *m_sensor;
 };
 
 #endif /* RECITATIONS_H */
