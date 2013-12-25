07cb85c7192346745ccd9f59623c91a4c822764a
Mohammed Sameer <msameer@foolab.org>
2013-12-25 21:02:03 +0200

    Update copyright years

--- common/aboutdata.cpp
+++ common/aboutdata.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2012 Mohammed Sameer <msameer@foolab.org>.
+ * Copyright (c) 2011-2013 Mohammed Sameer <msameer@foolab.org>.
  *
  * This package is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,7 +27,7 @@ AboutData::~AboutData() {
 }
 
 QString AboutData::copyright() const {
-  return tr("Copyright &copy; 2011-2012 <a href=\"mailto:msameer@foolab.org\">Mohammed Sameer</a>.");
+  return tr("Copyright &copy; 2011-2013 <a href=\"mailto:msameer@foolab.org\">Mohammed Sameer</a>.");
 }
 
 QString AboutData::appLicense() const {
--- common/main.cpp
+++ common/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2012 Mohammed Sameer <msameer@foolab.org>.
+ * Copyright (c) 2011-2013 Mohammed Sameer <msameer@foolab.org>.
  *
  * This package is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
