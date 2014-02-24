31cdff8a9420f594f30d467886de8baec976fa20
Mohammed Sameer <msameer@foolab.org>
2014-02-24 04:06:49 +0200

    pass command line arguments to libav configure script

--- build-libav.sh
+++ build-libav.sh
@@ -4,7 +4,7 @@ mkdir -p libav
 
 pushd libav
 
- ../../libav/configure --disable-programs --disable-doc  --disable-avdevice --disable-swscale --disable-network  --disable-everything --enable-sram --enable-bsf=mp3_header_decompress --enable-demuxer=mp3 --enable-decoder=mp3 --enable-pic
+ ../../libav/configure --disable-programs --disable-doc  --disable-avdevice --disable-swscale --disable-network  --disable-everything --enable-sram --enable-bsf=mp3_header_decompress --enable-demuxer=mp3 --enable-decoder=mp3 --enable-pic $@
 
 make
 
