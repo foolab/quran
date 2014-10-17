1ecd77ae39d0cd6dee5ab3876c83dc49b7d4bc51
Mohammed Sameer <msameer@foolab.org>
2014-10-17 03:29:31 +0300

    Initial android building script

---
+++ android.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+set -e
+
+export ANDROID_NDK_ROOT=/mnt/4/android/android-ndk-r10b
+export ANDROID_SDK_ROOT=/mnt/4/android/android-sdk-linux/
+export TOOLCHAIN=$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86/bin/
+export SYSROOT=$ANDROID_NDK_ROOT/platforms/android-9/arch-arm/
+export CC=$TOOLCHAIN/arm-linux-androideabi-gcc
+export CXX=$TOOLCHAIN/arm-linux-androideabi-g++
+export NM=$TOOLCHAIN/arm-linux-androideabi-nm
+export AR=$TOOLCHAIN/arm-linux-androideabi-ar
+export LD=$TOOLCHAIN/arm-linux-androideabi-ld
+export CFLAGS=--sysroot=$SYSROOT
+export CPPFLAGS=--sysroot=$SYSROOT
+export CXXFLAGS=--sysroot=$SYSROOT
+
+mkdir -p android/libav
+mkdir -p android/sqlite
+mkdir -p android/quazip
+
+pushd android/libav
+../../libav/configure --disable-programs --disable-doc  --disable-avdevice --disable-swscale \
+    --disable-network  --disable-everything --enable-sram --enable-bsf=mp3_header_decompress \
+    --enable-demuxer=mp3 --enable-decoder=mp3 --enable-pic --disable-static --enable-shared \
+    --enable-cross-compile --sysroot=$SYSROOT --target-os=linux --arch=arm \
+    --cross-prefix=$TOOLCHAIN/arm-linux-androideabi- \
+    --extra-cflags="-march=armv7-a -mfloat-abi=softfp" \
+    --extra-ldflags="-Wl,--fix-cortex-a8" --disable-symver
+sed -e 's/SHFLAGS.*/SHFLAGS="-shared"/' -i config.mak
+make
+popd
+
+pushd android/sqlite
+FLAGS="-DSQLITE_ENABLE_FTS4=1 $CFLAGS" \
+    ../../sqlite/configure --enable-readline=no \
+    --enable-threadsafe=no --enable-shared=yes --enable-static=no --enable-dynamic-extensions=no \
+    --host=arm-linux-androideabi --with-sysroot=$SYSROOT
+sed -e 's/\$soname/libsqlite.so/' -i libtool
+make
+popd
+
+pushd android/quazip
+/mnt/4/android/qt5.3/5.3/android_armv7/bin/qmake ../../quazip
+make
+popd
+
+pushd android
+/mnt/4/android/qt5.3/5.3/android_armv7/bin/qmake
+make
+popd
+
+pushd android
+make install INSTALL_ROOT=../apk
+popd
+
+/mnt/4/android/android-sdk-linux/tools//android update project --path apk \
+    --target android-19 --name Quran
+
+/mnt/4/android/qt5.3/5.3/android_armv7/bin/androiddeployqt \
+    --input android/android-libandroid-quran.so-deployment-settings.json \
+    --output apk \
+    --deployment bundled \
+    --verbose
+
+pushd apk
+ant installd
+popd
