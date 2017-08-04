8fb849674223713573dc04f9d9dcbc4abf4b4b04
Mohammed Sameer <msameer@foolab.org>
2017-08-05 01:41:27 +0300

    data/translations: Reworked gen.py

--- data/translations/gen.py
+++ data/translations/gen.py
@@ -2,8 +2,8 @@
 
 import zipfile
 import sys
-
-info = []
+import requests
+import pyquery
 
 def fix(line):
     parts = line.split('=')
@@ -29,14 +29,64 @@ def fileInfo(file):
 #    id language country direction file localizedName
     return inf
 
-for file in sys.argv[1:]:
-    info.append(fileInfo(file))
+def get_info(files):
+    info = []
+    for file in files:
+        info.append(fileInfo(file))
+    return info
 
-out = ["id", "language", "country", "direction", "file", "localizedName"]
-for i in info:
-    print "["+i['id']+"]"
-    for o in out:
-        print o + " = " + i[o]
+def write_output(info):
+    out = ["id", "language", "country", "direction", "file", "localizedName"]
 
-    print ""
+    output = []
+    for i in info:
+        output.append("["+i['id']+"]")
+        for o in out:
+            output.append(o + " = " + i[o])
+        output.append("")
 
+    open("trans.ini", "w").write("\n".join(output))
+
+def is_valid(a):
+    return "href" in a.attrib \
+           and "title" in a.attrib \
+           and a.attrib["title"] == "Download" \
+           and "class" in a.attrib \
+           and a.attrib["class"] == "icon download"
+
+def get_filename(header):
+    # This is a very simple parser
+    i = header.find("filename=")
+    file = header[i:].split('=')[1]
+    i = file.find(".zip")
+    file = file[:i+4]
+    return file
+
+def download(links):
+    files = []
+    for link in links:
+        r = requests.get(link + "&type=zekr")
+        file = get_filename(r.headers["Content-Disposition"])
+        open(file, "w").write(r.content)
+        print file
+        files.append(file)
+    return files
+
+def main():
+    files = []
+    if len(sys.argv) > 1:
+        files = sys.argv[1:]
+    else:
+        r = requests.get("http://tanzil.net/trans/")
+        if r.status_code != 200:
+            sys.exit('Failed to download index: %s' % r.reason)
+        p = pyquery.PyQuery(r.text)
+        links = []
+        links.extend([a.attrib["href"] for a in p('a') if is_valid(a)])
+        files = download(links)
+
+    info = get_info(files)
+    write_output(info)
+
+if __name__ == '__main__':
+    main()
